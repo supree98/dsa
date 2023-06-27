422ab940ceec39de72c087c10f450bc5f5060476
supree98 <supreethpgowda007@gmail.com>
2023-06-27 12:01:39 +0530

    Linked list operations

---
+++ src/main/java/com/spooky/learnbay/linkedlist/LinkedListOperations.java
@@ -0,0 +1,81 @@
+package com.spooky.learnbay.linkedlist;
+
+public class LinkedListOperations {
+
+    public Node insertAtBeginning(Node head, int data) {
+        Node node = new Node(data);
+        node.next = head;
+        head = node;
+        return head;
+    }
+
+    public void insertAtEnd(Node head, int data) {
+        Node node = new Node(data);
+        if (head == null) {
+            head = node;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = node;
+        }
+    }
+
+    public void delete(Node head, int data) {
+        Node temp = head;
+
+        while (temp.next != null && temp.next.data != data) {
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+    }
+
+    public Node reverse(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        Node prev = null;
+        Node current = head;
+        Node next = head.next;
+
+        while (next != null) {
+            current.next = prev;
+            prev = current;
+            current = next;
+            next = current.next;
+        }
+        current.next = prev;
+        return current;
+    }
+
+    public void print(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "-->");
+            temp = temp.next;
+        }
+    }
+
+    // test
+    public static void main(String[] args) {
+        LinkedListOperations lo = new LinkedListOperations();
+        Node head = new Node(10);
+//        head = lo.insertAtBeginning(head, 20);
+//        head = lo.insertAtBeginning(head, 30);
+//        head = lo.insertAtBeginning(head, 40);
+
+        lo.insertAtEnd(head, 20);
+        lo.insertAtEnd(head, 30);
+        lo.insertAtEnd(head, 40);
+        lo.insertAtEnd(head, 50);
+        lo.insertAtEnd(head, 60);
+        lo.print(head);
+        System.out.println();
+        head = lo.reverse(head);
+        lo.print(head);
+//        System.out.println();
+//        lo.delete(head, 50);
+//        lo.print(head);
+    }
+}
---
+++ src/main/java/com/spooky/learnbay/linkedlist/Node.java
@@ -0,0 +1,10 @@
+package com.spooky.learnbay.linkedlist;
+
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
