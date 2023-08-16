ef13965b2494e4a02dedb80ef3c38a752bb70c53
supree98 <supreethpgowda007@gmail.com>
2023-08-16 13:08:28 +0530

    Solve Binary Tree problems

---
+++ src/main/java/com/spooky/leetcode/tree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,34 @@
+package com.spooky.leetcode.tree;
+
+public class MaximumDepthOfBinaryTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+}
