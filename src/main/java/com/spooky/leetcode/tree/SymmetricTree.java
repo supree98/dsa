26ad3d23b42196130404877faac09044c5fe1513
supree98 <supreethpgowda007@gmail.com>
2023-08-16 17:06:38 +0530

    Solve 101. SymmetricTree

---
+++ src/main/java/com/spooky/leetcode/tree/SymmetricTree.java
@@ -0,0 +1,42 @@
+package com.spooky.leetcode.tree;
+
+public class SymmetricTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetric(root.left, root.right);
+    }
+
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || left.val != right.val) {
+            return false;
+        }
+        boolean sameLeft = isSymmetric(left.left, right.right);
+        boolean sameRight = isSymmetric(left.right, right.left);
+        return sameLeft && sameRight;
+    }
+}
