e9aaabfd5d96f8b187a443c91c2305f452b68369
supree98 <supreethpgowda007@gmail.com>
2023-08-16 16:50:44 +0530

    Solve 226. Invert Binary Tree

---
+++ src/main/java/com/spooky/leetcode/tree/InvertBinaryTree.java
@@ -0,0 +1,50 @@
+package com.spooky.leetcode.tree;
+
+public class InvertBinaryTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        InvertBinaryTree invertBinaryTree = new InvertBinaryTree();
+        invertBinaryTree.invertTree(root);
+    }
+}
