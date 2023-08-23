7da33ed5add4b04d68e7a31390acb6cffa688b7f
supree98 <supreethpgowda007@gmail.com>
2023-08-23 19:56:19 +0530

    Solve LowestCommonAncestorOfBinaryTree

---
+++ src/main/java/com/spooky/leetcode/tree/LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,56 @@
+package com.spooky.leetcode.tree;
+
+public class LowestCommonAncestorOfBinaryTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+            return "val: " + val;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // it is not root.val == p.val OR root.val == q.val
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode leftNode = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightNode = lowestCommonAncestor(root.right, p, q);
+
+        if (leftNode != null && rightNode != null) {
+            return root;
+        }
+        return leftNode == null ? rightNode : leftNode;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+        root.right = new TreeNode(1);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+        System.out.println(new LowestCommonAncestorOfBinaryTree().lowestCommonAncestor(root, root.left, root.left.right));
+    }
+
+}
