002b8ab7acdde06cf86d6e89dbb65ef73b77dc76
supree98 <supreethpgowda007@gmail.com>
2023-08-27 16:50:35 +0530

    Solve 189. Rotate Array

---
+++ src/main/java/com/spooky/leetcode/array/RotateArray.java
@@ -0,0 +1,41 @@
+package com.spooky.leetcode.array;
+
+public class RotateArray {
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+
+        // TRICK:
+        // Reverse the entire array
+        // Reverse the first k elements
+        // Reverse the remaining n - k elements
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        RotateArray solution = new RotateArray();
+        solution.rotate(nums, k);
+
+        System.out.print("Rotated array: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+
+}
