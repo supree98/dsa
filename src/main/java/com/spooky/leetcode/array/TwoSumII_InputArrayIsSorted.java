67d54f97ad510a7d917ffa6249888ed20da93120
supree98 <supreethpgowda007@gmail.com>
2023-08-28 12:32:58 +0530

    Solve 167. Two Sum II - Input Array Is Sorted

---
+++ src/main/java/com/spooky/leetcode/array/TwoSumII_InputArrayIsSorted.java
@@ -0,0 +1,22 @@
+package com.spooky.leetcode.array;
+
+public class TwoSumII_InputArrayIsSorted {
+
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[0];
+    }
+
+}
