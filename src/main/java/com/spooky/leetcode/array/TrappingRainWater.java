c71ededc5bb22d59b1357dcea623a3b5642aaa68
supree98 <supreethpgowda007@gmail.com>
2023-08-29 11:49:33 +0530

    Solve TrappingRainWater

---
+++ src/main/java/com/spooky/leetcode/array/TrappingRainWater.java
@@ -0,0 +1,32 @@
+package com.spooky.leetcode.array;
+
+public class TrappingRainWater {
+
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int waterTrapped = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {          // equal because if 2 blocks are same, water can not be trapped
+                    leftMax = height[left];
+                } else {
+                    waterTrapped += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    waterTrapped += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+
+        return waterTrapped;
+    }
+}
