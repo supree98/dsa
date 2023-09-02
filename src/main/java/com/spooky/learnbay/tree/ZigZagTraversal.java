246ad95ca9dcccea75bfdbceccf4e36ef5c990d4
supree98 <supreethpgowda007@gmail.com>
2023-09-02 20:09:28 +0530

    Solve ZigZagTraversal

---
+++ src/main/java/com/spooky/learnbay/tree/ZigZagTraversal.java
@@ -0,0 +1,12 @@
+package com.spooky.learnbay.tree;
+
+// Use a HashMap and store lvl -> node/val
+// print
+public class ZigZagTraversal {
+
+    // Refer PrintNodeAndLvl (from the map print the val in zigzag -> odd key and even key)
+
+    // If we use iteration then, while printing add the elements of that lvl to stack and print in
+    // zigzag else print normally
+
+}
