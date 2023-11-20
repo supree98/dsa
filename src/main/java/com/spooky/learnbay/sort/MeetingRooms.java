15e3aab5783d3ed572eefbb3530f4b81a252b6e7
supree98 <supreethpgowda007@gmail.com>
2023-11-20 19:46:38 +0530

    Solve DSA

---
+++ src/main/java/com/spooky/learnbay/sort/MeetingRooms.java
@@ -0,0 +1,34 @@
+package com.spooky.learnbay.sort;
+
+import java.util.Arrays;
+
+public class MeetingRooms {
+
+    public boolean canScheduleMeetings(int[][] meetings) {
+        if (meetings == null || meetings.length == 0) {
+            return true;  // No meetings to schedule, so it's possible.
+        }
+
+        Arrays.sort(meetings, (a, b) -> Integer.compare(a[0], b[0]));
+        // Arrays.sort(meetings, Comparator.comparingInt(a -> a[0]));
+
+        for (int i = 1; i < meetings.length; i++) {
+            if (meetings[i - 1][1] > meetings[i][0]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[][] meetings = {
+                {1, 3},
+                {2, 4},
+                {5, 7}
+        };
+
+        MeetingRooms meetingRooms = new MeetingRooms();
+        System.out.println(meetingRooms.canScheduleMeetings(meetings));
+    }
+}
