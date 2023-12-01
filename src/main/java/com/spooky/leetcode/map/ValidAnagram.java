310a847912fd6e68aa35b7651a067f719290d68a
supree98 <supreethpgowda007@gmail.com>
2023-12-01 12:00:51 +0530

    Solve DSA

---
+++ src/main/java/com/spooky/leetcode/map/ValidAnagram.java
@@ -0,0 +1,39 @@
+package com.spooky.leetcode.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        // store the count as values for each char in map
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+        }
+
+        if (sMap.size() != tMap.size()) {
+            return false;
+        }
+
+        for (Map.Entry<Character, Integer> entry : sMap.entrySet()) {
+            if (!entry.getValue().equals(tMap.get(entry.getKey()))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
