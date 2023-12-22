44649d25891485903c671a31c2fa57d44a5bf291
supree98 <supreethpgowda007@gmail.com>
2023-12-22 11:51:17 +0530

    Solve DSA

--- src/main/java/com/spooky/leetcode/tree/SumOfLeftLeaves.java
+++ src/main/java/com/spooky/leetcode/tree/SumOfLeftLeaves.java
@@ -3,25 +3,18 @@ package com.spooky.leetcode.tree;
 public class SumOfLeftLeaves {
 
     public int sumOfLeftLeaves(TreeNode root) {
-        return sumOfLeftLeaves(root, true);
-    }
-
-    public int sumOfLeftLeaves(TreeNode node, boolean isLeftLeafChild) {
-        if (node == null) {
+        if (root == null) {
             return 0;
         }
 
-        int leftSum = sumOfLeftLeaves(node.left, false);
-        int rightSum = sumOfLeftLeaves(node.right, true);
-
-        if (node.left == null && node.right == null) {
-            if (isLeftLeafChild) {
-                return 0;
-            } else {
-                return node.val;
-            }
+        int sum = 0;
+        // check if the left node is a leaf node and update the sum
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val;
         }
-        return leftSum + rightSum;
+        sum += sumOfLeftLeaves(root.left);
+        sum += sumOfLeftLeaves(root.right);
+        return sum;
     }
 
     public class TreeNode {
